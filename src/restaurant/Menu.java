89fe7304e7dffcfb806e05c885426da625b08bbf
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-05-17 13:18:57 -0500

    Added getters and setters to Menu and MenuItem classes.

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -1,4 +1,23 @@
 package restaurant;
 
+import java.util.ArrayList;
+
 public class Menu {
+    private ArrayList<MenuItem> menu;
+    public Menu(ArrayList<MenuItem> menu) {
+        this.menu = menu;
+    }
+    public Menu() {
+        this.menu = new ArrayList<>();
+    }
+    public ArrayList<MenuItem> getMenus() {
+        return menu;
+    }
+    public void setMenu(ArrayList<MenuItem> menu) {
+        this.menu = menu;
+    }
+    public void addMenuItem(MenuItem aMenuItem) {
+        this.menu.add(aMenuItem);
+    }
 }
+
--- src/restaurant/MenuItem.java
+++ src/restaurant/MenuItem.java
@@ -1,4 +1,43 @@
 package restaurant;
 
+//import java.util.Arrays;
+import java.util.Date;
+
 public class MenuItem {
+    public String foodName;
+    public double price;
+    public String description;
+    public String category = null;
+    public boolean isNew = true;
+    private Date dateAdded;
+
+    //Setters
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    // Getters
+    public String getFoodName() {
+        return foodName;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getCategory() {
+        return category;
+    }
+
 }
+
