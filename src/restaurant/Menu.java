536e9a4c0baa05b0d9a35c6a8ce0442b5a6c81ac
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-05-20 11:03:51 -0500

    Reworked Menu, MenuItems and Restaurant to be able to add and remove menu items, print the whole menu, and print individual menu items.

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -1,23 +1,57 @@
 package restaurant;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Arrays;
 
 public class Menu {
-    private ArrayList<MenuItem> menu;
-    public Menu(ArrayList<MenuItem> menu) {
-        this.menu = menu;
+    private Date dateLastUpdated;
+
+    private ArrayList<String> categories = new ArrayList<>(
+            Arrays.asList("appetizer", "main course", "dessert")
+    );
+
+    private ArrayList<MenuItem> items;
+
+    public Menu(ArrayList<MenuItem> items) {
+        this.items = items;
+        this.dateLastUpdated = new Date();
     }
-    public Menu() {
-        this.menu = new ArrayList<>();
+
+    // Setters
+//    public void setMenu(ArrayList<MenuItem> items) {
+//        this.items = items;
+//    }
+
+    // Getters
+    public ArrayList<MenuItem> getItems() {
+        return items;
     }
-    public ArrayList<MenuItem> getMenus() {
-        return menu;
+
+    public ArrayList<String> getCategories() {
+        return categories;
     }
-    public void setMenu(ArrayList<MenuItem> menu) {
-        this.menu = menu;
+
+    public Date getDateLastUpdated() {
+        return dateLastUpdated;
     }
-    public void addMenuItem(MenuItem aMenuItem) {
-        this.menu.add(aMenuItem);
+
+
+    // Method to add menu item
+    public void addMenuItem(MenuItem item) {
+        this.items.add(item);
+    }
+
+    // Method to delete menu item
+    public void removeMenuItem(MenuItem item){
+        this.items.remove(item);
+    }
+
+    public void printMenu() {
+        System.out.println("Restaurant Menu");
+        for (MenuItem menuItem : items) {
+            System.out.println(menuItem.toString() + "\n");
+        }
     }
 }
 
